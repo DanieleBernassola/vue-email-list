2b63e391575e68e98490def10780566ace84bf99
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-28 17:36:40 +0200

    [UPD] Added emails array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-"use strict";
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      emails: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        console.log(response.data);
+        this.emails = response.data;
+        console.log(this.emails);
+      });
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        console.log(response.data);
+        this.emails = response.data;
+        console.log(this.emails);
+      });
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        console.log(response.data);
+        this.emails = response.data;
+        console.log(this.emails);
+      });
+  },
+}).mount("#app");
